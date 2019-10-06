ee5b85a200fd61a502af99bdd089421b874108c1
Tony Wu <theambiguousninja@gmail.com>
2019-10-06 16:35:38 -0700

    basic functionality

--- script.js
+++ script.js
@@ -1,29 +1,124 @@
+// Constants
 const MS_IN_DAY = 1000 * 24 * 60 * 60;
+const referenceDate = new Date(2019, 7, 20);
 
-const rootApp = document.getElementById('label');
-
-
-rootApp.innerHTML = `${shift(modDays())}`
-
-
-function modDays() {
-	const referenceDate = new Date(2019, 7, 20);
-	const diff = ((new Date() - referenceDate)/MS_IN_DAY) % 8;
-	return Math.floor(diff);
+function Duration(ms) {
+	this.seconds = Math.floor(ms / 1000) % 60;
+	this.minutes = Math.floor(ms / (1000 * 60)) % 60;
+	this.hours = Math.floor(ms / (1000 * 60 * 60)) % 60;
 }
 
-function shift(modDay) {
-	if (modDay > 3) {
-		return "Joe does not work today";
-	} else if (modDay > 1) {
-		return "Joe is working a night shift today";
-	} else if (modDay >= 0) {
-		return "Joe is working a day shift today";
+const status = document.getElementById('work-status');
+const timestamp = document.getElementById('timestamp');
+
+var loop = setInterval(function () {
+	const now = new Date();
+	const cycleIndex = workCycleIndex(now);
+ 	const hours = now.getHours();
+
+	if (cycleIndex > 4) {
+		const offset = 8 - cycleIndex;
+
+		const diffDate = offsetDate(now, offset);
+		diffDate.setHours(7, 0, 0);
+		updateLabels(false, diffDate - now);
+	} else if (cycleIndex == 4) {
+		if (hours < 7) {
+			const diffDate = new Date(now);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(true, diffDate - now);
+		} else {
+			const diffDate = offsetDate(now, 4);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(false, diffDate - now);
+		}
+	} else if (cycleIndex == 3) {
+		if (hours < 19 && hours >= 7) {
+			const diffDate = new Date(now);
+			diffDate.setHours(19, 0, 0);
+			updateLabels(false, diffDate - now);
+		} else if (hours >= 19) {
+			const diffDate = offsetDate(now, 1);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(true, diffDate - now);
+		} else {
+			const diffDate = new Date(now);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(true, diffDate - now);
+		}
+	} else if (cycleIndex == 2) {
+		if (hours >= 19) {
+			const diffDate = offsetDate(now, 1);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(true, diffDate - now);
+		} else if (hours < 19  && hours >= 7) {
+			const diffDate = new Date(now);
+			diffDate.setHours(19, 0, 0);
+			updateLabels(false, diffDate - now);
+		} else {
+			const diffDate = new Date(now);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(true, diffDate - now);
+		}
+	} else if (cycleIndex == 1) {
+		if (hours < 7) {
+			const diffDate = new Date(now);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(false, diffDate - now);
+		} else if (hours >= 19) {
+			const diffDate = new Date(now);
+			diffDate.setHours(19, 0, 0);
+			updateLabels(false, diffDate - now);
+		} else {
+			const diffDate = new Date(now);
+			diffDate.setHours(19, 0, 0);
+			updateLabels(true, diffDate - now);
+		}
 	} else {
-		return "Joe is stretching";
+		if (hours < 7) {
+			const diffDate = new Date(now);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(false, diffDate - now);
+		} else if (hours >= 19) {
+			const diffDate = offsetDate(now, 1);
+			diffDate.setHours(7, 0, 0);
+			updateLabels(false, diffDate - now);
+		} else {
+			const diffDate = new Date(now);
+			diffDate.setHours(19, 0, 0);
+			updateLabels(true, diffDate - now);
+		}
+	}
+}, 1000);
+
+// Methods =====================================================================
+function updateLabels(isWorking, delta) {
+	if (isWorking) {
+		status.innerHTML = "Yes, Joe is at work!";
+		timestamp.innerHTML = "He'll be done in " + durationString(delta);
+	} else {
+		status.innerHTML = "Nope, Joe isn't at work!";
+		timestamp.innerHTML = "His next shift will be in " + durationString(delta);
 	}
 }
 
-function buttonClick() {
-	rootApp.innerHTML = "Hi"
+// Returns the date offset from today
+function offsetDate(date, offsetDays) {
+	var offsetDate = new Date(date);
+	offsetDate.setDate(offsetDate.getDate() + offsetDays);
+	return offsetDate;
+}
+
+// Returns the date in workCycle
+function workCycleIndex(date) {
+	var diffDate = new Date(date);
+	diffDate.setHours(0, 0, 0, 0)	// Set to midnight
+	const diff = Math.ceil(((diffDate - referenceDate)/MS_IN_DAY));
+	return diff % 8;
+}
+
+// Converts a duration to a duration string
+function durationString(ms) {
+	const duration = new Duration(ms);
+	return duration.hours + " hours " + duration.minutes + " minutes " + duration.seconds + " seconds";
 }
