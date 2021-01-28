3aaf5a6847b9122700ca8621522ed78d6e69b99d
Tony Wu <theambiguousninja@gmail.com>
2021-01-28 06:45:01 -0800

    fix joe's schedule

--- script.js
+++ script.js
@@ -6,28 +6,30 @@ const shift = {
 
 // Constants
 const MS_IN_DAY = 1000 * 24 * 60 * 60;
-const referenceDate = new Date(2019, 7, 20);
+const referenceDate = new Date(2021, 0, 23);
 
 const status = document.getElementById('work-status');
 const timestamp = document.getElementById('timestamp');
 const label1 = document.getElementById('label1');
 
-var loop = setInterval(function() {
-	cycleIndex = cycleIndex(daysSinceStart());
-	type = shiftType(cycleIndex);
-	
-	switch (type) {
-		case shift.NONE:
-			status.innerHTML = "Joe is not working today";
-			break;
-		case shift.DAY:
-			status.innerHTML = "Joe is working a day shift today";
-			break;
-		case shift.NIGHT:
-			status.innerHTML = "Joe is working a night shift today";
-			break;
-	}
-}, 1000);
+doThing();
+
+function doThing() {
+    cycleIndex = cycleIndex(daysSinceStart());
+    type = shiftType(cycleIndex);
+    
+    switch (type) {
+        case shift.NONE:
+            status.innerHTML = "Joe is not working today";
+            break;
+        case shift.DAY:
+            status.innerHTML = "Joe is working a day shift today";
+            break;
+        case shift.NIGHT:
+            status.innerHTML = "Joe is working a night shift today";
+            break;
+    }
+}
 
 function daysSinceStart() {
 	ms = new Date() - referenceDate
